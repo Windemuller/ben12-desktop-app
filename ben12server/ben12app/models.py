87426f7f225206d77f068c9b7bad2f24f5886843
Stephan <stephanwindemuller@gmail.com>
2023-06-05 12:27:42 +0200

    Add models to the app

--- ben12server/ben12app/models.py
+++ ben12server/ben12app/models.py
@@ -1,3 +1,76 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Client(models.Model):
+    id = models.IntegerField()
+    name = models.CharField(default="No name", max_length=50)
+    date_of_birth = models.DateTimeField("Date of Birth")
+    date_created = models.DateTimeField("Date of account creation")
+    last_measurement = models.DateTimeField("DateTime of last record")
+    last_time_alcohol_consumed = models.DateTimeField("Last alcohol consumption")
+    address = models.CharField(default="No address", max_length=50)
+
+    def __str__(self):
+        return f"Client {self.id} name: {self.name}"
+
+
+class Record(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    heartbeat = models.IntegerField(default=-1)
+    blood_oxygen_level = models.FloatField(default=-1)
+    alcohol_level = models.FloatField(default=-1)
+    date_time_recording = models.DateTimeField("Time of data recording")
+    date_time_sent = models.DateTimeField("Time of data transmission")
+
+    def __str__(self):
+        return f"Record for {self.client}, recorded {self.date_time_recording}, sent {self.date_time_sent}"
+
+
+class RunningAverageDay(models.Model):
+    client = models.OneToOneField(
+        Client,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    alcohol_level = models.FloatField(default=-1)
+    blood_oxygen_level = models.FloatField(default=-1)
+    heartrate = models.IntegerField(default=-1)
+    num_records = models.IntegerField(default=0)
+    date_last_updated = models.DateTimeField("Date of last update")
+
+    def __str__(self):
+        return f"Day average, last update {self.date_last_updated}, {self.client}"
+
+
+class RunningAverageWeek(models.Model):
+    client = models.OneToOneField(
+        Client,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    alcohol_level = models.FloatField(default=-1)
+    blood_oxygen_level = models.FloatField(default=-1)
+    heartrate = models.IntegerField(default=-1)
+    num_records = models.IntegerField(default=0)
+    date_last_updated = models.DateTimeField("Date of last update")
+
+    def __str__(self):
+        return f"Week average, last update {self.date_last_updated}, client {self.client}"
+
+
+class RunningAverageMonth(models.Model):
+    client = models.OneToOneField(
+        Client,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    alcohol_level = models.FloatField(default=-1)
+    blood_oxygen_level = models.FloatField(default=-1)
+    heartrate = models.IntegerField(default=-1)
+    num_records = models.IntegerField(default=0)
+    date_last_updated = models.DateTimeField("Date of last update")
+
+    def __str__(self):
+        return f"Month average, last update {self.date_last_updated}, client {self.client}"
