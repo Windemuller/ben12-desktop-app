216c28ca795f0f66a51399f1566e4f714f5a42b1
Stephan <stephanwindemuller@gmail.com>
2023-06-13 15:41:49 +0200

    Change double quotes to single quotes

--- ben12server/ben12server/urls.py
+++ ben12server/ben12server/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("app/", include("ben12app.urls")),
+    path('app/', include("ben12app.urls")),
     path('api-auth/', include('rest_framework.urls')),
 ]
