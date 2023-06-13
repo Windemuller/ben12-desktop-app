9a8d64fb052b18e255c44484385c7b021cb7c103
Stephan <stephanwindemuller@gmail.com>
2023-06-13 15:50:38 +0200

    Update urls to include new client detail view

--- ben12server/ben12app/urls.py
+++ ben12server/ben12app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/clients', views.ClientAPIView.as_view(), name="index"),
+    path('api/clients', views.ClientAPIView.as_view()),
+    path('api/clients/<int:client_id>', views.ClientDetailView.as_view()),
 ]
