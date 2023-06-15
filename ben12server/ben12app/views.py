1bb01c17347dedd377712c19b42787fced7e3111
Stephan <stephanwindemuller@gmail.com>
2023-06-15 14:36:12 +0200

    Remove unused import

--- ben12server/ben12app/views.py
+++ ben12server/ben12app/views.py
@@ -2,7 +2,6 @@ from datetime import datetime
 
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework import status
 from .serializers import *
 
