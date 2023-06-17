4d1ea04fc6df1ea27575c8484245a5945b371a64
Stephan <stephanwindemuller@gmail.com>
2023-06-17 17:30:38 +0200

    Add Record view with GET and POST endpoints

--- ben12server/ben12app/urls.py
+++ ben12server/ben12app/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/clients', views.ClientAPIView.as_view()),
+    path('api/clients', views.ClientGeneralView.as_view()),
     path('api/clients/<int:client_id>', views.ClientDetailView.as_view()),
+    path('api/records/<int:client_id>', views.RecordDetailView.as_view()),
+    path('api/records/<int:client_id>/<int:record_id>', views.RecordDetailView.as_view()),
 ]
--- ben12server/ben12app/views.py
+++ ben12server/ben12app/views.py
@@ -6,7 +6,7 @@ from rest_framework import status
 from .serializers import *
 
 
-class ClientAPIView(GenericAPIView):
+class ClientGeneralView(GenericAPIView):
     serializer_class = ClientSerializer
 
     def get(self, request, *args, **kwargs) -> Response:
@@ -30,17 +30,18 @@ class ClientAPIView(GenericAPIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+def get_client(client_id: int) -> Client or None:
+    try:
+        return Client.objects.get(id=client_id)
+    except Client.DoesNotExist:
+        return None
+
+
 class ClientDetailView(GenericAPIView):
     serializer_class = ClientSerializer
 
-    def get_client(self, client_id: int) -> Client or None:
-        try:
-            return Client.objects.get(id=client_id)
-        except Client.DoesNotExist:
-            return None
-
     def get(self, request, client_id: int, *args, **kwargs) -> Response:
-        client_instance = self.get_client(client_id)
+        client_instance = get_client(client_id)
         if not client_instance:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
@@ -48,9 +49,62 @@ class ClientDetailView(GenericAPIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request, client_id: int, *args, **kwargs) -> Response:
-        client_instance = self.get_client(client_id)
+        client_instance = get_client(client_id)
         if not client_instance:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         client_instance.delete()
         return Response(status=status.HTTP_200_OK)
+
+
+class RecordDetailView(GenericAPIView):
+    serializer_class = RecordSerializer
+
+    def get(self, request, client_id: int, how_many: int = 10, *args, **kwargs) -> Response:
+        client_instance = get_client(client_id)
+        if not client_instance:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            records = Record.objects.get(client=client_instance).order_by("date_time_recording")[:how_many]
+        except Record.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = RecordSerializer(records)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def get_specific(self, request, client_id: int, record_id: int, *args, **kwargs) -> Response:
+        client_instance = get_client(client_id)
+        if not client_instance:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            records = Record.objects.get(client=client_instance, id=record_id)
+        except Record.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = RecordSerializer(records)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, client_id: int, *args, **kwargs) -> Response:
+        client_instance = get_client(client_id)
+
+        if not client_instance:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        data = {
+            'client': client_instance,
+            'heartbeat': request.data.get('heartbeat'),
+            'blood_oxygen_level': request.data.get('blood_oxygen_level'),
+            'alcohol_level': request.data.get('alcohol_level'),
+            'date_time_recording': request.data.get('date_time_recording'),
+            'date_time_sent': datetime.now()
+        }
+
+        serializer = RecordSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
