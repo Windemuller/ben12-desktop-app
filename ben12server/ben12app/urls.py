f4fa379cf14341a80170ee7335665302ad57daba
Stephan <stephanwindemuller@gmail.com>
2023-06-19 17:13:37 +0200

    Fix bug where GET for record gave an error

--- ben12server/ben12app/urls.py
+++ ben12server/ben12app/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('api/clients', views.ClientGeneralView.as_view()),
     path('api/clients/<int:client_id>', views.ClientDetailView.as_view()),
     path('api/records/<int:client_id>', views.RecordDetailView.as_view()),
-    path('api/records/<int:client_id>/<int:record_id>', views.RecordDetailView.as_view()),
 ]
